ac5d043b87c80e04e3d5fb73719184a1687e0ce2
hAWKdv <hawk94@abv.bg>
2015-11-07 23:08:36 +0200

    Deleted del.js

--- del.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var jclass = require("./src/jclass.js");
-
-var Object = jclass.create(function() {})
-        .func("toString", function () {});
-
-console.log(Object.Obj.prototype.toString.name);
